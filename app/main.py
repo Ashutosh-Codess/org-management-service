3c394ea37f97800e0fc1ea94b33d3d5817b8f630
unknown <ashu19022004.com>
2025-12-13 01:55:36 +0530

    Add working root route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+﻿from fastapi import FastAPI
 from app.routers.org_routes import router as org_router
 from app.routers.org_create import router as create_router
 from app.routers.admin_auth import router as admin_router
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Org Management Service Running"}
+    return {"message": "Org Management Service Running!"}
 
 app.include_router(org_router)
 app.include_router(create_router)
