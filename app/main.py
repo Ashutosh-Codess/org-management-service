23d2b2561c90b3b1928784e8c8f765b2d1c342c5
unknown <ashu19022004.com>
2025-12-13 01:30:02 +0530

    Fix all path imports for Render deployment

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
-
 from app.routers.org_routes import router as org_router
 from app.routers.org_create import router as create_router
 from app.routers.admin_auth import router as admin_router
 
 app = FastAPI()
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+@app.get("/")
+def root():
+    return {"message": "Org Management Service Running"}
 
-app.include_router(create_router)
 app.include_router(org_router)
+app.include_router(create_router)
 app.include_router(admin_router)
